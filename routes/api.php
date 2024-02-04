696be2c11bddfa8b79ec7b38cf9d66f6af903331
kyeongsookim <zpla15@naver.com>
2024-02-04 21:55:55 +0900

    add fitness inquiry route, model

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FitnessConsultController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -16,4 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::prefix("v1")->group(function () {
+    Route::post('/how-to-lose-weight', [FitnessConsultController::class, 'fitnessInquiry']);
 });
